69caba1e9eea07fee57a1f5deca851cd99c93dac
vaishali161197 <vaishali161197vs@gmail.com>
2021-01-29 21:48:10 +0530

    Number_Wizard_UI

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadNextScreen()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+    public void LoadStartScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+
+}
